5df3208bc44ef2f29ff7c44170bca9cedc62f4ac
DESKTOP-8IF5MAA\USUARIO <alberto11fdez@gmail.com>
2023-03-13 14:06:23 +0100

    Proyecto creado con base de datos

---
+++ Estebanco/src/main/java/es/estebanco/estebanco/EstebancoApplication.java
@@ -0,0 +1,13 @@
+package es.estebanco.estebanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstebancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstebancoApplication.class, args);
+	}
+
+}
